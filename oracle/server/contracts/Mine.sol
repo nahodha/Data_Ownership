b87be50537be8822d40c0f9fb8e378ff61182482
Elvis Lutta <eolutta2@gmail.com>
2018-10-15 16:01:10 +0300

    Added Factories for contract creation.

--- oracle/server/contracts/Mine.sol
+++ oracle/server/contracts/Mine.sol
@@ -1,48 +1,101 @@
 pragma solidity ^0.4.24;
 
+contract MineFactory {
+    address[] public deployedMineContracts;
+
+    function createMineContract() public {
+        address newMineContract = new Mine();
+        deployedMineContracts.push(newMineContract);
+    }
+
+    function getDeployedMineContracts()
+    public
+    view
+    returns(address[])
+    {
+        return deployedMineContracts;
+    }
+}
+
+
 contract Mine {
 
-    address public miner;
-    address[] public minee;
+    address public miner = address(0);
+    // address[100] public dataOwners;
+    mapping(address => bool) public dataOwners;
+    uint dataOwnersCounter = 0;
+    mapping(uint => address) public dOwnAddresses; // used for looping through addresses
+    bool minerAvailable;
+    bool dataMined = false;
 
-    modifier mineesOnly() {
-        require(msg.sender != miner, "Only minees are allowed to invoke this function");
+    constructor () public {
+    }
+
+    modifier dataOwnerOnly(address owner) {
+        require(dataOwners[owner] == true, "Only dataOwners are allowed to invoke this function");
         _;
     }
 
     modifier minerOnly() {
-        require(msg.sender == miner, "Only miners are allowed to invoke this function");
+        require(msg.sender == miner, "Only Miners are allowed to invoke this function");
         _;
     }
 
-    constructor () public {
-        miner = msg.sender;
+    function addMiner(address minerAddress) public returns (address) {
+        require(!minerAvailable, "Miner already exist's in this contract create a new one.");
+
+        miner = minerAddress;
+        minerAvailable = true;
+
+        return miner;
     }
 
-    function mine() public payable minerOnly {
-        // Require at leas .01 ether to mine users data.
-        require(msg.value >= .001 ether, "You need at least 0.001 ether to mine!");
+    function allowMine() public returns (bool) {
+        require(!dataOwners[msg.sender], "You can only register once");
+        require(msg.sender != miner, "Miners cannot be dataOwners");
+
+        dataOwners[msg.sender] = true;
+        dOwnAddresses[dataOwnersCounter] = msg.sender;
+        dataOwnersCounter++;
+
+        return true;
     }
 
-    function allowMine() public mineesOnly {
-        minee.push(msg.sender);
+    function startMine()
+        public
+        payable
+        minerOnly
+        returns (bool)
+    {
+        // Require at least .01 ether to mine users data.
+        require(msg.value > .01 ether, "You require at least 0.01 ether to mine.");
+
+        dataMined = true;
+
+        return true;
     }
 
-    function mined() public mineesOnly {
+    function payDataOwners()
+        public
+        dataOwnerOnly(msg.sender)
+        returns (bool)
+    {
+        require(dataMined, "Data has already been mined.");
 
-        uint arrayLength = minee.length;
-
-        for (uint i = 0; i < arrayLength; i ++) {
-            minee[i].transfer(this.balance/arrayLength);
+        for (uint i = 0; i < dataOwnersCounter; i ++) {
+            dOwnAddresses[i].transfer(address(this).balance/dataOwnersCounter);
+            dataOwners[dOwnAddresses[i]] = false;
+            dOwnAddresses[i] = address(0);
         }
         // reset minee pool to make sure they are no longer in the array
-        minee = new address[](0);
+        dataOwnersCounter = 0;
+        // Set dataNotMined to false so that currency cannot be sent twice.
+        dataMined = false;
+        miner = address(0);
+
+        return true;
     }
 
-    function getMinees() public view returns (address[]) {
-        return minee;
-    }
-
-    function() public payable{}
+    function () public payable {}
 
 }
--- oracle/server/contracts/Sell.sol
+++ oracle/server/contracts/Sell.sol
@@ -1,26 +1,100 @@
 pragma solidity ^0.4.24;
 
+
+contract SellFactory {
+    address[] public deployedSellContracts;
+
+    function createSellContract() public {
+        address newSellContract = new Sell();
+        deployedSellContracts.push(newSellContract);
+    }
+
+    function getDeployedSellContracts()
+    public
+    view
+    returns(address[])
+    {
+        return deployedSellContracts;
+    }
+}
+
+
 contract Sell {
     address public buyer;
     address public seller;
+    bool notBought = false;
+    bool noBuyer = true;
+    bool noSeller = true;
+
+    constructor () public {
+    }
 
     modifier buyerOnly() {
-        require(msg.sender == buyer, "Only buyers allowed to invoke this method");
+        require(msg.sender == buyer, "Only buyers can invoke this function.");
         _;
     }
 
-    // modifier sellerOnly() {
-    //     require(msg.sender == seller, "Only sellers are allowed to invoke this method");
-    //     _;
-    // }
-
-    function setSeller(address _buyer) public {
-        seller = msg.sender;
-        buyer = _buyer;
+    modifier sellerOnly() {
+        require(msg.sender == seller, "Only sellers can invoke this function.");
+        _;
     }
 
-    function buy() public payable buyerOnly{
-        require(msg.value > 0.05 ether, "You need at least 0.0005 ether to invoke this function");
-        seller.transfer(msg.value);
+    modifier bothParties() {
+        require(msg.sender == seller || msg.sender == buyer, "Only buyer or seller can invoke this action.");
+        _;
     }
+
+    modifier buyerNotSeller() {
+        require(buyer != seller, "buyer cannot be seller.");
+        _;
+    }
+
+    function addBuyer(address _seller) public returns(bool) {
+        require(noBuyer, "buyer already exists");
+        require(noSeller, "A seller already exists");
+        buyer = msg.sender;
+        seller = _seller;
+        notBought = true;
+
+        require(buyer != seller, "buyer cannot be seller.");
+
+        noBuyer = false;
+        noSeller = false;
+
+        return true;
+    }
+
+    function buy()
+            public
+            payable
+            buyerOnly
+            buyerNotSeller
+            returns (bool)
+    {
+        require(msg.value > 0.0005 ether, "You require at least 0.0005 ether to call this function.");
+        require(notBought, "Cannot sell twice.");
+
+        seller.transfer(address(this).balance);
+
+        // reset buyers and sellers
+        cancelTransaction();
+
+        return true;
+    }
+
+    function cancelTransaction()
+        public
+        bothParties
+        returns(bool)
+    {
+        buyer = address(0);
+        seller = address(0);
+        noBuyer = true;
+        noSeller = true;
+        notBought = false;
+
+        return true;
+    }
+
+    function () public payable {}
 }
