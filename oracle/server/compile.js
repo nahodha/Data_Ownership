8dac2f24751fcfa81455ed908c9b1c03668d6e4b
Elvis Lutta <eolutta2@gmail.com>
2018-10-15 19:48:23 +0300

    Modified compile.js

--- oracle/server/compile.js
+++ oracle/server/compile.js
@@ -1,14 +1,34 @@
 const path = require('path'),
-      fs = require('fs'),
+      fs = require('fs-extra'),
       solc = require('solc');
 
+// Create build directory if it does not exist
+const buildPath = path.resolve(__dirname, 'build');
+
+// fs.ensureDirSync(__dirname + './build', (err) => {
+//   if (err)
+//     console.error('Directory could not be created!\n\n' + err);
+
+  fs.removeSync(buildPath);
+// });
 
 const minePath = path.resolve(__dirname, 'contracts', 'Mine.sol'),
-      sellPath = path.resolve(__dirname, 'contracts', 'Sell.sol'),
-      sourcePath = path.resolve(__dirname, 'contracts', 'Contracts.sol');
+      sellPath = path.resolve(__dirname, 'contracts', 'Sell.sol');
 
 const mineSource = fs.readFileSync(minePath, 'utf8'),
-      sellSource = fs.readFileSync(sellPath, 'utf8'),
-      source = fs.readFileSync(sourcePath, 'utf8');
+      sellSource = fs.readFileSync(sellPath, 'utf8');
 
-module.exports = solc.compile(sellSource, 1).contracts[':Sell'];
+let mineOutput = solc.compile(mineSource, 1).contracts;
+let sellOutput = solc.compile(sellSource, 1).contracts;
+
+let outputs = [mineOutput, sellOutput];
+
+fs.ensureDirSync(buildPath);
+for (let i = 0; i < outputs.length; i++) {
+  for (let contract in outputs[i]) {
+    fs.outputJSONSync(
+      path.resolve(buildPath, contract.replace(':', '') + '.json'),
+      outputs[i][contract]
+    );
+  }
+}
