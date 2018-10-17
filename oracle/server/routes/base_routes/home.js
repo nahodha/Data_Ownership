4184c0a62395fe3c59a8e1218bba0553dc673fe9
Elvis Lutta <eolutta2@gmail.com>
2018-10-17 19:54:46 +0300

    Added some gui for oracle

--- oracle/server/routes/base_routes/home.js
+++ oracle/server/routes/base_routes/home.js
@@ -28,7 +28,7 @@ router.post('/allowMine', async (req, res) => {
     let user = await User.findById(req.body.userId).exec();
 
     if (!user) {
-      return res.send({ success: false, message: 'Your user does\'nt exist' });
+      return res.send({ success: false, message: 'Your user doesn\'t exist' });
     }
 
     // Unlock accounts but make sure password is correct
@@ -97,7 +97,7 @@ router.post('/mine', async (req, res) => {
 
     let mined = await mine.methods.startMine()
       .send({
-        value: web3.utils.toWei('0.1', 'ether'),
+        value: web3.utils.toWei('0.6', 'ether'),
         from: minerAccount.address,
         gas: '1000000'
       });
@@ -135,6 +135,163 @@ router.post('/payOwners', async (req, res) => {
       res.send({ success: true, message: 'Successfully paid' });
     } else {
       res.send({ success: false, message: 'Failed to pay owners' });
+    }
+  } else {
+    res.status(403).send('You\'re not supposed to be here');
+  }
+});
+
+
+// GUI ROUTES
+
+router.get('/addMinerGui', (req, res) => {
+  const errors = req.flash('error');
+  const success = req.flash('success');
+  const messages = [];
+
+  messages.push(errors);
+  messages.push(success);
+
+  res.render('base_routes/addMiner', {
+    apiKey: process.env.MINE_API_KEY,
+    hasErrors: errors.length > 0,
+    hasSuccess: success.length > 0,
+    messages: messages
+  });
+});
+
+router.post('/addMinerGui', async (req, res) => {
+  if (req.query.apiKey == process.env.MINE_API_KEY) {
+    let minerAccount = await Account.findOne({ address: req.body.address }).exec();
+
+    // Unlock accounts
+    if (req.body.password != process.env.VENDOR_PASSWORD) {
+      return res.redirect('/addMinerGui');
+    }
+    web3.eth.personal.unlockAccount(minerAccount.address, process.env.VENDOR_PASSWORD, process.env.UNLOCK_TIME);
+
+    // Get contract assume that address will always be passed as argument for now
+    let mine = await MineContract(minerAccount.address);
+
+    // Add dataowner to the mining pool
+    let addMiner = await  mine.methods.addMiner(minerAccount.address)
+      .send({
+        from: minerAccount.address,
+        gas: '1000000'
+      });
+
+    if (addMiner) {
+      req.flash('success', 'Successfully added Miner');
+      return res.redirect('/addMinerGui');
+    } else {
+      req.flash('error', 'Failed to add miner!');
+      return res.redirect('/addMinerGui');
+    }
+  } else {
+    res.status(403).send('You\'re not supposed to be here');
+  }
+});
+
+
+router.get('/mineGui', (req, res) => {
+  const errors = req.flash('error');
+  const success = req.flash('success');
+  const messages = [];
+
+  messages.push(errors);
+  messages.push(success);
+
+  res.render('base_routes/startMine', {
+    apiKey: process.env.MINE_API_KEY,
+    hasErrors: errors.length > 0,
+    hasSuccess: success.length > 0,
+    messages: messages
+  });
+});
+
+router.post('/mineGui', async (req, res) => {
+  if (req.query.apiKey == process.env.MINE_API_KEY) {
+    const Vendor = require('../../models/Vendor');
+    let vendor = await Vendor.findOne({ vendorName: 'vendo' }).exec();
+    if (!vendor || req.body.password != process.env.VENDOR_PASSWORD) {
+      return res.redirect('/mineGui');
+    }
+    let minerAccount = await Account.findOne({ owner: vendor.id }).exec();
+
+    console.log(minerAccount);
+    // Unlock accounts
+    web3.eth.personal.unlockAccount(minerAccount.address, process.env.VENDOR_PASSWORD, process.env.UNLOCK_TIME);
+
+    // Get contract assume that address will always be passed as argument for now
+    let mine = await Mine(req.body.contractAddress);
+
+    let mined = await mine.methods.startMine()
+      .send({
+        value: web3.utils.toWei('0.6', 'ether'),
+        from: minerAccount.address,
+        gas: '1000000'
+      });
+
+    if (mined) {
+      req.flash('success', 'Successfully Mined contract');
+      return res.redirect('/mineGui');
+    } else {
+      req.flash('error', 'Failed to Mined contract');
+      return res.redirect('/mineGui');
+    }
+
+    res.send({ success: true, message: 'Successful mine' });
+  } else {
+    res.status(403).send('You\'re not supposed to be here');
+  }
+});
+
+
+router.get('/payOwnersGui', (req, res) => {
+  const errors = req.flash('error');
+  const success = req.flash('success');
+  const messages = [];
+
+  messages.push(errors);
+  messages.push(success);
+
+  res.render('base_routes/payOwners', {
+    apiKey: process.env.MINE_API_KEY,
+    hasErrors: errors.length > 0,
+    hasSuccess: success.length > 0,
+    messages: messages
+  });
+});
+
+
+router.post('/payOwnersGui', async (req, res) => {
+  if (req.query.apiKey == process.env.MINE_API_KEY) {
+    let user = await User.findOne({email: req.body.email}).exec();
+
+    if (!user || !user.validUserPassword(req.body.password)) {
+      return res.redirect('/payOwnersGui');
+    }
+    let mineeAccount = await Account.findOne({ owner: user.id }).exec();
+
+    // Unlock accounts
+    web3.eth.personal.unlockAccount(mineeAccount.address, req.body.password, process.env.UNLOCK_TIME);
+
+    // Get contract assume that address will always be passed as argument for now
+    let mine = await Mine(req.body.contractAddress);
+
+    // Add dataowner to the mining pool
+    let payOwners = await  mine.methods.payDataOwners()
+      .send({
+        from: mineeAccount.address,
+        gas: '1000000'
+      });
+
+    if (payOwners) {
+      req.flash('success', 'Successfully Paid owners!');
+      return res.redirect('/payOwnersGui');
+    } else {
+      req.flash('error', 'Failed to Pay Owners');
+      return res.redirect('/payOwnersGui');
     }
   } else {
     res.status(403).send('You\'re not supposed to be here');
